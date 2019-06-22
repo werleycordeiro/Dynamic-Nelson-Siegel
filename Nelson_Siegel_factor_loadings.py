19fe88d87d6857291c8284fcc77db70eefebc838
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-06-22 10:37:55 -0300

    Create Nelson_Siegel_factor_loadings.py

---
+++ Nelson_Siegel_factor_loadings.py
@@ -0,0 +1,8 @@
+#Loading matrix
+
+def Nelson_Siegel_factor_loadings(lam,m):
+ c1 = np.ones(m.shape[0])
+ c2 = (1-np.exp(-lam*m))/(lam*m)
+ c3 = c2-np.exp(-lam*m)
+ lambmat = np.vstack((c1,c2,c3)).T
+ return(lambmat)
