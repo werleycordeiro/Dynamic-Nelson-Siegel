1ce11bff97240b5b9c1bd06f6f2012cb82f8ff54
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-07-24 10:27:38 -0300

    Update DNS-TS.py

--- DNS-TS.py
+++ DNS-TS.py
@@ -17,7 +17,7 @@ df.tail();
 
 l=df.shape[0]
 
-m=np.array([3,6,9,12,15,18,21,24,30,36,48,60,72,84,96,108,120])
+matu=np.array([3,6,9,12,15,18,21,24,30,36,48,60,72,84,96,108,120])
 
 T=m.shape[0]
 
@@ -28,12 +28,12 @@ lam=0.0609
 # os.getcwd()
 # os.chdir("")
 import Nelson_Siegel_factor_loadings as Nelson_Siegel_factor_loadings 
-z = Nelson_Siegel_factor_loadings(lam=lam,m=m)
+Z = Nelson_Siegel_factor_loadings(lam=lam,matu=matu)
 
 # Betas and Yhat
 
 import Yhat_betas as Yhat_betas
-results = Yhat_betas(Y=df,Z=z)
+results = Yhat_betas(Y=df,Z=Z)
 results[0].head()
 results[1].head()
 
