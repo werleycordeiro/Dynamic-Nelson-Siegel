bcf549f4874a5503d5cb2d17d11a70d3a8ca4a92
werleycordeiro <31543812+werleycordeiro@users.noreply.github.com>
2019-07-24 10:30:32 -0300

    Update Nelson_Siegel_factor_loadings.py

--- Nelson_Siegel_factor_loadings.py
+++ Nelson_Siegel_factor_loadings.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 
-def Nelson_Siegel_factor_loadings(lam,m):
- c1 = np.ones(m.shape[0])
- c2 = (1-np.exp(-lam*m))/(lam*m)
- c3 = c2-np.exp(-lam*m)
+def Nelson_Siegel_factor_loadings(lam,matu):
+ c1 = np.ones(matu.shape[0])
+ c2 = (1-np.exp(-lam*matu))/(lam*matu)
+ c3 = c2-np.exp(-lam*matu)
  lambmat = np.vstack((c1,c2,c3)).T
  return(lambmat)
